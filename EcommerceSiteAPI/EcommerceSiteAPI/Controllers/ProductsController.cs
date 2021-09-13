5a7b98008ded35ee91e6a2bcee39d845823bdebb
ThiagoNascimentoItu <thiago.nascimento57@etec.sp.gov.b>
2021-09-13 16:41:42 -0300

    criação do projeto

---
+++ EcommerceSiteAPI/EcommerceSiteAPI/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using EcommerceSiteAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EcommerceSiteAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : Controller
+    {
+        private List<Product> _product = new List<Product>
+        { 
+            new Product(1,"Chinelo",10),
+            new Product(2,"Tempero",5),
+            new Product(3,"Bicicleta",50),
+            new Product(4,"Raiva",250),
+        };
+
+       [HttpGet]
+       public IActionResult Get()
+        {
+            return Ok(_product);
+        }
+    }
+}
